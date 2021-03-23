b31792807e2fca8f0794bb7ef8085e629b2ea1dd
sinbowon <tlsqhdnjs55@gmail.com>
2021-03-23 11:19:32 +0900

    코인 게임 첫번째 커밋

---
+++ hhhhh.py
@@ -0,0 +1,12 @@
+ import random
+  random.random()
+  0.90389642027948769
+abc =['e', 'd', 'a', 'c', 'b']
+random.choice(abc)
+'a'
+random.choice(abc)
+'d'
+
+menu = '쫄면', '육계장', '비빔밥'
+random.choice(menu)
+'쫄면'
